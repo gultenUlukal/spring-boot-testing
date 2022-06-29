d2e5dc1293c3b65050662af69ada1bcf791c63de
gultenUlukal <guelten.ulukal@hubject.com>
2022-06-29 10:18:52 +0200

    Create serviceImpl class

--- src/main/java/com/gulukal/springboottesting/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/gulukal/springboottesting/service/impl/EmployeeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.gulukal.springboottesting.service.impl;
 
+import com.gulukal.springboottesting.exception.ResourceNorFoundException;
 import com.gulukal.springboottesting.model.Employee;
 import com.gulukal.springboottesting.repository.EmployeeRepository;
 import com.gulukal.springboottesting.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -17,6 +20,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+        if(savedEmployee.isPresent()){
+            throw new ResourceNorFoundException("Employee already exist with given email:" + employee.getEmail());
+        }
         return employeeRepository.save(employee);
     }
 }
